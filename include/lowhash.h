ff4ece7e958d7c9cad68f3edf3fa9e2ded06c099
Levent Kaya <levent@dev>
2025-06-28 18:16:30 +0300

    [feature] basic api interfaced

--- include/lowhash.h
+++ include/lowhash.h
@@ -2,24 +2,35 @@
 #define LOWHASH_H_
 
 #include <stdbool.h>
+#include <stddef.h>
 
-typedef struct lh_item {
+typedef struct lowhash_node {
   void *key;
   void *value;
-} lh_item_t;
+  int hash_code;
+  struct lowhash_node *next;
+} lowhash_node_t;
 
 typedef struct lowhash {
-  lh_item_t *hash;
+  lowhash_node_t **buckets;
+  size_t bucket_size;
+  size_t num_buckets;
+
+  void (*clear)(lowhash_t *hash);
+  bool (*contains)(lowhash_t *hash, void *key);
+  bool (*is_empty)(lowhash_t *hash);
+  void *(*get)(lowhash_t *hash, void *key);
+  void *(*get_or_default)(lowhash_t *hash, void *key, void *def_val);
+  bool (*put)(lowhash_t *hash, void *key, void *value);
+  bool (*remove)(lowhash_t *hash, void *key);
+  void (*destroy)(lowhash_t *hash);
 } lowhash_t;
 
 /**
- * @brief      beggining of the public api
+ * @brief      Public api Functions
  */
-void clear(lowhash_t *hash);
-bool contains(lowhash_t *hash, void *value);
-bool is_empty(lowhash_t *hash);
-void *get(void *key);
-void *get_or_default(void *key, void *def_val);
-void put(lowhash_t *hash, void *key, void *value);
+lowhash_t *lowhash_create(size_t initial_capacity, hash_func_t hash_func,
+                          key_equals_func_t key_equals);
+void lowhash_destroy(lowhash_t *hash);
 
 #endif // LOWHASH_H_
--- src/lowhash.c
+++ src/lowhash.c
@@ -1,7 +1,58 @@
 #include <lowhash.h>
-#include <stdio.h>
+#include <lowhash_config.h>
 
-int main(int argc, char *argv[]) {
-  printf("hello, world\n");
-  return 0;
+/**
+ * @brief      Forward declarations of internal functions
+ *
+ * @details    See struct lowhash_t
+ */
+static void lowhash_clear_impl(lowhash_t *hash);
+static bool lowhash_contains_impl(lowhash_t *hash, void *key);
+static bool lowhash_is_empty_impl(lowhash_t *hash);
+static void *lowhash_get_impl(lowhash_t *hash, void *key);
+static void *lowhash_get_or_default_impl(lowhash_t *hash, void *key,
+                                         void *def_val);
+static bool lowhash_put_impl(lowhash_t *hash, void *key, void *value);
+static bool lowhash_remove_impl(lowhash_t *hash, void *key);
+static void lowhash_destroy_impl(lowhash_t *hash);
+static bool lowhash_resize(lowhash_t *hash);
+
+// ============================================================================
+// IMPLEMENTATION FUNCTIONS
+// ============================================================================
+static bool lowhash_is_empty_impl(lowhash_t *hash) {
+  return !hash || hash->bucket_size = 0;
+}
+
+// ============================================================================
+// PUBLIC API
+// ============================================================================
+lowhash_t *lowhash_create(size_t initial_capacity, hash_func_t hash_func,
+                          key_equals_func_t key_equals) {
+  if (initial_capacity == 0) {
+    initial_capacity = LOWHASH_DEFAULT_CAPACITY;
+  }
+
+  if (!hash_func || !key_equals) {
+    return NULL; // hash and equeals functions required
+  }
+
+  lowhash_t *hash = malloc(sizeof(lowhash_t));
+  if (!hash) {
+    return NULL;
+  }
+
+  hash->buckets = calloc(initial_capacity, sizeof(lowhash_node_t *));
+  if (!hash->buckets) {
+    free(hash);
+    return NULL;
+  }
+
+  hash->bucket_count = initial_capacity;
+  hash->bucket_size = 0;
+  hash->hash_func = hash_func;
+  hash->key_equals = key_equals;
+
+  // Assign function pointers
+  hash->is_empty = lowhash_is_empty_impl;
 }
